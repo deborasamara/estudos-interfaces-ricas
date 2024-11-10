de669b3cfd265c17eda80404d20639d745f4c582
Débora Rodrigues <50723433+deborasamara@users.noreply.github.com>
2024-11-10 23:28:11 +0000

    feat: implementação do calculo fatorial

---
+++ testes-unitarios/fatorial.js
@@ -0,0 +1,9 @@
+function fatorial(n){
+    if(n<0) throw new Error("O número precisa ser positivo para funcionar");
+    else if(n === 0 || n === 1)
+        return 1;
+    return n * fatorial(n - 1);
+    
+}
+
+module.exports = fatorial;
