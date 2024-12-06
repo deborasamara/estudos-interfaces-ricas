d41049e90825860b6ca326a7e4194412321e064d
Débora Rodrigues <50723433+deborasamara@users.noreply.github.com>
2024-12-06 13:22:18 +0000

    chore: mudança nas config

--- testes-unitarios/fatorial.js
+++ testes-unitarios/fatorial.js
@@ -1,9 +1,8 @@
-function fatorial(n){
-    if(n<0) throw new Error("O número precisa ser positivo para funcionar");
-    else if(n === 0 || n === 1)
+function fatorial(n) {
+    if (n < 0)
+        throw new Error("O número precisa ser positivo para funcionar");
+    else if (n === 0 || n === 1)
         return 1;
     return n * fatorial(n - 1);
-    
 }
-
-module.exports = fatorial;
+export default fatorial;
--- testes-unitarios/fatorial.test.js
+++ testes-unitarios/fatorial.test.js
@@ -1,11 +1,20 @@
-const fatorial = require('./fatorial');  
-
-// teste 1
-test('fatorial de 4 deve ser 24', () => {
-    expect(fatorial(4)).toBe(24);
+const fatorial = require('./fatorial');
+// teste 1 - numero negativo
+test('Fatorial de número negativo não deve ser aceito', () => {
+    expect(() => {
+        fatorial(0);
+    }).toThrow();
 });
-
-// teste 2
+// teste 2 - numero 1 (positivo)
+test('fatorial de 1 deve ser 1', () => {
+    expect(fatorial(1)).toBe(1);
+});
+// teste 3 - numero 0 (número neutro)
+test('fatorial de 0 deve ser 1', () => {
+    expect(fatorial(0)).toBe(1);
+});
+// teste 4 - numero 6 (positivo )
 test('fatorial de 6 deve ser 720', () => {
     expect(fatorial(6)).toBe(720);
 });
+export {};
