e0046ed93d93a1cd740ffe09b040eb7a9d1204f0
Débora Rodrigues <50723433+deborasamara@users.noreply.github.com>
2024-12-06 13:14:29 +0000

    refactor e feat: melhoramento do código e ajuste das configurações

---
+++ testes-unitarios/fatorial.ts
@@ -0,0 +1,9 @@
+
+function fatorial(n:number): number{
+    if(n<0) throw new Error("O número precisa ser positivo para funcionar");
+    else if(n === 0 || n === 1)
+        return 1;
+    return n * fatorial(n - 1);
+}
+
+export default fatorial;
