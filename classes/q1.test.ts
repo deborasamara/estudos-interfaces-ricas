4fce784c010323eea3f5993b659664748ab9e33b
Débora Rodrigues <50723433+deborasamara@users.noreply.github.com>
2025-01-17 13:06:07 +0000

    fix: correção do erro de teste de chamada de função

--- classes/q1.test.ts
+++ classes/q1.test.ts
@@ -4,14 +4,18 @@ import { Vaca, Sapo } from "./q1";
     const sapo = new Sapo('0.1m', 'Anfibio');
     sapo.alturaMediaDoPulo = '5 metros';
     expect(sapo.alturaMediaDoPulo).toBe('5 metros');
+    const emitirSomSpy = jest.spyOn(sapo, 'emitirSom');
     sapo.emitirSom();
     expect(sapo.emitirSom).toHaveBeenCalled();
+    emitirSomSpy.mockRestore();
  })
 
  test('Teste da classe vaca', ()=>{
     const vaca = new Vaca('Médio', 'Alguma classificacao ai');
     vaca.porte = 'Grande';
     expect(vaca.porte).toBe('Grande');
+    const emitirSomSpy = jest.spyOn(vaca, 'emitirSom');
     vaca.emitirSom();
     expect(vaca.emitirSom).toHaveBeenCalled();
+    emitirSomSpy.mockRestore();
  })
